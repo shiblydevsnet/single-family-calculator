096ea6181ab8a6c3f73e090d121dd45fb2d246fc
colag <shiblydu60@yahoo.com>
2021-03-01 22:44:06 +0600

    modified

---
+++ single-family-calculator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Single Family Calculator
+Plugin URI: https://
+Description: Calculation of single family
+Version: 1.0
+Author: User1
+License: GPLv2 or later
+Text Domain: create-post
+*/
